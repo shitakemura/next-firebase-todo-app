e9c7d6598d586f547e90e41522f2de710aa21130
shitakemura <shitakemura@gmail.com>
2022-02-02 21:28:00 +0900

    Introduct ChakraUI

---
+++ components/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { Stack, Text } from "@chakra-ui/react";
+
+export const Home = () => {
+  return (
+    <Stack>
+      <Text color='blue.500'>Home</Text>
+    </Stack>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Todo App</title>
+      </Head>
+      <main>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </main>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
